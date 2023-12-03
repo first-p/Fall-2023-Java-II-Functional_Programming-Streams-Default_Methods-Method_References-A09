96e2862a6d5c88985358f21c811c64e0447590eb
first-p <98608650+first-p@users.noreply.github.com>
2023-12-03 15:31:36 -0600

    created the construtor and run mehtod in the Prok09Runner.javafile, now testing

--- Proj09Runner.java
+++ Proj09Runner.java
@@ -1,2 +1,31 @@
+/***************************************************
+ File: Proj09runner.java
+ Author: Fred Butoma
+ Purpose: Proj09Runner.java is a class that has a
+ run method that sorts strings and integers from the
+ command line and returns a decscending ordered list
+ ***************************************************/
+import java.util.*;
+import java.util.stream.*;
+
 public class Proj09Runner {
+    private String[] data;
+
+    // Constructor for Proj07Runner
+    public Proj09Runner() {
+        // Print the certification statement
+        System.out.println("I certify that this program is my own work\n" +
+                "and is not the work of others. I agree not\n" +
+                "to share my solution with others.\n" +
+                "Fred Butoma");
+
+
+    }
+    public List<String> run(){
+        List<String> sortedList = Arrays.stream(data)
+                                        .sorted(Comparator.reverseOrder())
+                                        .collect(Collectors.toList());
+
+        return sortedList;
+    }
 }
